780399e39ea29fccf19fd4577477f03612dc9328
mternez <mert.ernez@gmail.com>
2025-04-01 21:20:44 +0200

    gap buffer OK (but needs some more testing)

--- src/data_structures/main.go
+++ src/data_structures/main.go
@@ -7,29 +7,12 @@ import (
 
 func main() {
 	a := gapbuffer.NewGapBuffer(50, 10)
-	a.Insert('1', 10)
-	a.PrintWithVisibleGap("After Insert '1' at 10")
-	a.Insert('2', 11)
-	a.PrintWithVisibleGap("After Insert '2' at 11")
-	a.Insert('3', 12)
-	a.PrintWithVisibleGap("After Insert '3' at 12")
-	a.Insert('6', 30)
-	a.PrintWithVisibleGap("After Insert '6' at 30")
-	a.Insert('7', 31)
-	a.PrintWithVisibleGap("After Insert '7' at 31")
-	a.Insert('8', 32)
-	a.PrintWithVisibleGap("After Insert '8' at 32")
-	a.Insert('9', 33)
-	a.PrintWithVisibleGap("After Insert '9' at 33")
-	a.Insert('A', 34)
-	a.PrintWithVisibleGap("After Insert 'A' at 34")
-	a.Insert('B', 35)
-	a.PrintWithVisibleGap("After Insert 'B' at 35")
-	a.Insert('C', 36)
-	a.PrintWithVisibleGap("After Insert 'C' at 36")
-	a.Insert('D', 36)
-	a.PrintWithVisibleGap("After Insert 'D' at 36")
-	a.Insert('E', 36)
-	a.PrintWithVisibleGap("After Insert 'D' at 36")
+	str := "Gap buffers are very cool, you should try them"
+	a.MoveCursor(6)
+	a.PrintWithVisibleGap("After moving cursor to position 6")
+	for _, r := range str {
+		a.Insert(r)
+		a.PrintWithVisibleGap("After inserting '" + string(r) + "'")
+	}
 	fmt.Println(string(a.Buffer()))
 }
